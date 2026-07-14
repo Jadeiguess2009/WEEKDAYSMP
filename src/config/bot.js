2ecb9996ee5d102c44dbe491ac166ce8d3c141be
Jadeiguess2009 <aislynnepatterson@gmail.com>
2026-07-14 04:04:52 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Counting or not counting weekdays...",     // this is what people actually see
+        state: "refreshing and delicous!",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
